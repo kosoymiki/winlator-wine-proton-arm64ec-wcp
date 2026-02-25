3bb5594261989533ed0d9c588901d260e67ed1f4
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 <kizya200272@gmail.com>
2026-02-25 19:24:25 +0300

    ci/validation: require explicit donor URLs in workflow env

--- ci/validation/check-urc-mainline-policy.sh
+++ ci/validation/check-urc-mainline-policy.sh
@@ -92,7 +92,9 @@ check_workflow_env_contract() {
   require_contains "${wf}" 'WCP_FEX_EXPECTATION_MODE: external'
   require_contains "${wf}" 'WCP_BIONIC_SOURCE_MAP_FORCE: "1"'
   require_contains "${wf}" 'WCP_BIONIC_SOURCE_MAP_REQUIRED: "1"'
+  require_contains "${wf}" 'WCP_BIONIC_LAUNCHER_SOURCE_WCP_URL:'
   require_contains "${wf}" 'WCP_BIONIC_LAUNCHER_SOURCE_WCP_SHA256:'
+  require_contains "${wf}" 'WCP_BIONIC_UNIX_SOURCE_WCP_URL:'
   require_contains "${wf}" 'WCP_BIONIC_UNIX_SOURCE_WCP_SHA256:'
 
   require_not_contains "${wf}" 'WCP_RUNTIME_CLASS_TARGET: glibc-wrapped'
